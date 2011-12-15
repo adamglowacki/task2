0547e25c96f28c0f4246993431713220939837ea
Adam Głowacki <adas@adam-hp.site>
2011-12-15 14:51:41 +0100

    Teraz grantAcl jest w pre-insert. Ale jest problem, bo znowu robi się sam flush sesji podczas flushowania i tym razem wygląda na to że 2 razy dodajemy tn sam obiekt (może o coś innego chodzi?).

--- src/main/java/pl/edu/mimuw/ag291541/task2/security/executor/AclGrant.java
+++ src/main/java/pl/edu/mimuw/ag291541/task2/security/executor/AclGrant.java
@@ -1,11 +1,10 @@
 package pl.edu.mimuw.ag291541.task2.security.executor;
 
-import org.hibernate.event.PostInsertEvent;
-import org.hibernate.event.PostInsertEventListener;
+import org.hibernate.event.PreInsertEvent;
+import org.hibernate.event.PreInsertEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 
 import pl.edu.mimuw.ag291541.task2.entity.Announcement;
 import pl.edu.mimuw.ag291541.task2.entity.AnnouncementInstance;
@@ -14,7 +13,7 @@ import pl.edu.mimuw.ag291541.task2.security.entity.User;
 import pl.edu.mimuw.ag291541.task2.security.service.ACLService;
 import pl.edu.mimuw.ag291541.task2.util.UserUtil;
 
-public class AclGrant implements PostInsertEventListener {
+public class AclGrant implements PreInsertEventListener {
 	private static final long serialVersionUID = 6157501295776229143L;
 
 	private Logger log = LoggerFactory.getLogger(AclGrant.class);
@@ -27,29 +26,31 @@ public class AclGrant implements PostInsertEventListener {
 	private DuringProcessingSet duringProcessingInListener;
 
 	@Override
-	@Transactional
-	// (propagation = Propagation.REQUIRES_NEW)
-	public void onPostInsert(PostInsertEvent event) {
+	// @Transactional(propagation = Propagation.REQUIRES_NEW)
+	public boolean onPreInsert(PreInsertEvent event) {
 		Object entity = event.getEntity();
 		User sender = userUtil.getUser();
 		if (sender != null) {
-			if (entity instanceof Announcement) {
-				// TransactionStatus txStatus = txManager.getTransaction(null);
-				// log.debug("New transaction: {}",
-				// txStatus.isNewTransaction());
-				Announcement a = (Announcement) entity;
-				// TransactionStatus txStatus =
-				// txManager.getTransaction(null);
-				log.debug("About to grant WRITE on {} to {}", a, sender);
-				aclService.addInstanceAccess(a, ACLRights.WRITE, sender);
-				// txStatus.flush();
-				// txManager.commit(txStatus);
-			} else if (entity instanceof AnnouncementInstance) {
-				if (!duringProcessingInListener.isDuringAccessCheck(entity)) {
-					try {
-						duringProcessingInListener.setDuringAccessCheck(entity);
+			if (!duringProcessingInListener.isDuringAccessCheck(entity)) {
+				try {
+					duringProcessingInListener.setDuringAccessCheck(entity);
+					if (entity instanceof Announcement) {
+						// TransactionStatus txStatus =
+						// txManager.getTransaction(null);
+						// log.debug("New transaction: {}",
+						// txStatus.isNewTransaction());
+						Announcement a = (Announcement) entity;
+						// TransactionStatus txStatus =
+						// txManager.getTransaction(null);
+						log.debug("About to grant WRITE on {} to {}", a, sender);
+						aclService
+								.addInstanceAccess(a, ACLRights.WRITE, sender);
+						// txStatus.flush();
+						// txManager.commit(txStatus);
+					} else if (entity instanceof AnnouncementInstance) {
 						AnnouncementInstance ai = (AnnouncementInstance) entity;
-						Announcement a = ai.getAnnouncement();
+						Object[] state = event.getState();
+						Announcement a = (Announcement) state[0];
 						User receiver = ai.getReceiver();
 						// TransactionStatus txStatus =
 						// txManager.getTransaction(null);
@@ -61,13 +62,14 @@ public class AclGrant implements PostInsertEventListener {
 								receiver);
 						aclService.addInstanceAccess(a, ACLRights.READ,
 								receiver);
-					} finally {
-						duringProcessingInListener.clear(entity);
+						// txStatus.flush();
+						// txManager.commit(txStatus);
 					}
+				} finally {
+					duringProcessingInListener.clear(entity);
 				}
-				// txStatus.flush();
-				// txManager.commit(txStatus);
 			}
 		}
+		return false;
 	}
 }
